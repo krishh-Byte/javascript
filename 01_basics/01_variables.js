2af2c432917b08c00ecf8d3f3218ad0993e097aa
krishh-Byte <parmarkrishna935@gmail.com>
2024-09-03 18:13:27 +0000

    about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const age=20
+let height=50
+var weight=90
+city ="bengaluru"
+// age=29 not allowed
+console.table([age,height,weight,city]);
+/* prefer not to use the keyword var because of its scope problem*/
