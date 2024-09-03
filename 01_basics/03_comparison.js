71f85c3164a7183ee5ecdb03395c98c36eb949d9
krishh-Byte <parmarkrishna935@gmail.com>
2024-09-03 18:16:48 +0000

    comparison operator

---
+++ 01_basics/03_comparison.js
@@ -0,0 +1,30 @@
+// console.log("3"==3);
+// console.log(null==1);
+// console.log(undefined==null);
+// console.log(null==0);
+// console.log(null>=0); 
+// console.log(undefined>=0);
+// console.log(null===null);
+// console.log(undefined==="");
+// console.log(undefined=="");
+// console.log(null==="0");
+// console.log(null==0);
+// console.log("2"===3);
+// console.log(undefined===null);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(+true)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
